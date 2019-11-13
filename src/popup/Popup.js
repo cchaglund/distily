52811cc41eb0843a35a826ba8c21ad2049878f9f
Christofer Haglund <christofer.haglund@outlook.com>
2019-11-13 15:12:06 +0100

    added project title input field

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,24 +1,74 @@
-import React from 'react';
+/* eslint-disable no-undef */
+
+import React, { useState } from 'react';
 import './Popup.css';
 
-const TechStackLogos = () => {
-  return (
-    <div className="tech-logos">
-      <img className="logo" src="/img/react.svg" alt="ReactJS logo" title="ReactJS" />
-      <img className="logo" src="/img/webpack.svg" alt="Webpack logo" title="Webpack" />
-      <img className="logo" src="/img/eslint.svg" alt="ESLint logo" title="ESLint" />
-      <img className="logo" src="/img/jest.svg" alt="Jest logo" title="Jest" />
-    </div>
-  );
-};
+
+// // Get the saved stats and render the data in the popup window.
+// const MAX_ITEMS = 5;
+
+// function sorter(array) {
+//   return Object.keys(array).sort((a, b) => {
+//     return array[a] <= array[b];
+//   });
+// }
+
+// function addElements(element, array, callback) {
+//   while(element.firstChild) {
+//     element.removeChild(element.firstChild);
+//   }
+
+//   for (let i=0; i < array.length; i++) {
+//     if (i >= MAX_ITEMS) {
+//       break;
+//     }
+
+//     const listItem = document.createElement('li');
+//     listItem.textContent = callback(array[i]);
+//     element.appendChild(listItem);
+//   }
+// }
+
+// const gettingStoredStats = browser.storage.local.get();
+
+// gettingStoredStats.then(results => {
+//   if (results.type.length === 0 || results.host.length === 0) {
+//     return;
+//   }
+
+//   let hostElement = document.getElementById('hosts');
+//   let sortedHosts = sorter(results.host);
+//   addElements(hostElement, sortedHosts, (host) => {
+//     return `${host}: ${results.host[host]} visit(s)`;
+//   });
+
+//   let typeElement = document.getElementById('types');
+//   let sortedTypes = sorter(results.type);
+//   addElements(typeElement, sortedTypes, (type) => {
+//     return `${type}: ${results.type[type]} use(s)`;
+//   });
+
+// });
 
 const Popup = () => {
+  const [ projectName, setProjectName ] = useState();
+
+  const changeHandler = () => {
+    setProjectName( e.target.value );
+  };
+
   return (
     <div className="popup">
-      <p className="popup-greet">Thanks for using <span className="brand">Modern extension Boilerplate</span></p>
-      <p className="stack-head">Made using :</p>
-      <TechStackLogos />
-      <p className="contrib-msg">We would love some of your help in making this boilerplate even better. <br /><a href="https://www.github.com/kryptokinght/react-extension-boilerplate" target="_blank">React Extension Boilerplate</a></p>
+      <div
+        onClick={ () => console.log('create clicked')}>
+          Create new project
+      </div>
+      <input 
+        type="text" 
+        value={ projectName } 
+        onChange={ () => changeHandler }>
+        { projectName }
+      </input>
     </div>
   );
 };
